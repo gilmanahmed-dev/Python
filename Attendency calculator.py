02a598615ee72570d106702d67070d3037219865
Gilman Ahmed <gilmanahmed.dev@gmail.com>
2026-06-12 21:54:34 +0600

    Attendency calculator to calculate attencency rate

---
+++ Attendency calculator.py
@@ -0,0 +1,15 @@
+
+print("Attendency Calculator")
+print("---------------------")
+
+subject = input("Enter course name : ")
+total_class = int(input("Enter the number of total classes : "))
+attended = int(input("How many classes did you attend ? "))
+rate = (attended/total_class)*100
+
+print(f" In {subject} course \n Total class days = {total_class} \n You have attended {attended} classes \n Your attencency rate is {rate}%")
+
+if rate < 60:
+    print(" You can't sit for the exam!")
+elif rate > 60:
+    print(" You have enough attencency rate to sit for the exam")
