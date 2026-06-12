a2b3ef5d7466c4d5f2b9fad87fc5e06997497f5f
Gilman Ahmed <gilmanahmed.dev@gmail.com>
2026-06-13 00:49:09 +0600

    number guessing game

--- guess the num.py
+++ guess the num.py
@@ -4,14 +4,14 @@ attempts = 0
 
 while True:
     guess = int(input("Guess the number between 1 to 100 : "))
+    attempts += 1
+    
     if num == guess:
         print("You won !")
         break
     elif guess < num:
         print("Think higher")
-        attempts += 1
     elif guess > num:
         print("Think lower")
-        attempts += 1
 
 print(f"Total attempts = {attempts}")
